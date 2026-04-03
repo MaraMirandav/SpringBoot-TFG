4d9b5e0e24910c6351740dde3bbacc8583d6a3e9
Maria Elena Miranda <mara.mirandav@gmail.com>
2026-04-03 21:18:20 +0200

    Modificación en los inserts y consultas

--- src/main/resources/bbdd/selects.sql
+++ src/main/resources/bbdd/selects.sql
@@ -5,7 +5,7 @@ SELECT
     u.first_name AS "Nombre",
     u.alias AS "Alias",
     s.sex AS "Sexo",
-    d.nivel_dependency AS "Nivel Dependencia"
+    d.dependency_level AS "Nivel Dependencia"
 FROM schema_template.users u
 JOIN schema_template.sex_enum s ON u.sex_id = s.id
 JOIN schema_template.user_dependency_enum d ON u.dependency_id = d.id;
@@ -69,8 +69,8 @@ SELECT
     c.comment AS "Solución"
 FROM schema_template.cd_incidents i
 JOIN schema_template.incident_cd_enum cat ON i.incident_cd_id = cat.id
-JOIN schema_template.significance_cd_enum sig ON i.significance_cd_id = sig.id
-JOIN schema_template.workers w_creator ON i.created_by_worker_id = w_creator.id
+JOIN schema_template.significances_cd_enum sig ON i.significance_cd_id = sig.id
+JOIN schema_template.workers w_creator ON i.reported_by_id = w_creator.id
 LEFT JOIN schema_template.incidents_cd_comments c ON c.cd_incident_id = i.id
 LEFT JOIN schema_template.workers w_comment ON c.worker_id = w_comment.id
 ORDER BY i.id;
@@ -88,9 +88,9 @@ SELECT
     c.comment AS "Texto Comentario"
 FROM schema_template.users_incidents i
 JOIN schema_template.users u ON i.user_id = u.id
-JOIN schema_template.incident_user_enum cat ON i.incident_user_id = cat.id
-JOIN schema_template.significance_user_enum sig ON i.significance_user_id = sig.id
-JOIN schema_template.workers w_creator ON i.created_by_worker_id = w_creator.id
+JOIN schema_template.incidents_user_enum cat ON i.incident_user_id = cat.id
+JOIN schema_template.significances_user_enum sig ON i.significance_user_id = sig.id
+JOIN schema_template.workers w_creator ON i.reported_by_id = w_creator.id
 LEFT JOIN schema_template.incidents_users_comments c ON c.user_incident_id = i.id
 LEFT JOIN schema_template.workers w_comment ON c.worker_id = w_comment.id
 ORDER BY i.created_at DESC, c.created_at ASC;
@@ -107,23 +107,20 @@ SELECT
     c.comment AS "Respuesta"
 FROM schema_template.cd_incidents i
 JOIN schema_template.incident_cd_enum cat ON i.incident_cd_id = cat.id
-JOIN schema_template.significance_cd_enum sig ON i.significance_cd_id = sig.id
-JOIN schema_template.workers w_creator ON i.created_by_worker_id = w_creator.id
+JOIN schema_template.significances_cd_enum sig ON i.significance_cd_id = sig.id
+JOIN schema_template.workers w_creator ON i.reported_by_id = w_creator.id
 LEFT JOIN schema_template.incidents_cd_comments c ON c.cd_incident_id = i.id
 LEFT JOIN schema_template.workers w_comment ON c.worker_id = w_comment.id
 ORDER BY i.created_at DESC, c.created_at ASC;
 
 ------------------
--- Roles y posiciones
+-- Roles
 
 SELECT
     w.id,
     CONCAT(w.first_name, ' ', w.first_surname) AS nombre_completo,
-    STRING_AGG(DISTINCT p.position_name, ' - ') AS cargos,
     STRING_AGG(DISTINCT r.role_name, ' - ') AS roles_app
 FROM schema_template.workers w
-LEFT JOIN schema_template.workers_positions wp ON w.id = wp.worker_id
-LEFT JOIN schema_template.positions_enum p ON wp.position_id = p.id
 LEFT JOIN schema_template.workers_roles wr ON w.id = wr.worker_id
 LEFT JOIN schema_template.roles_enum r ON wr.role_id = r.id
 GROUP BY w.id, w.first_name, w.first_surname
@@ -138,7 +135,7 @@ SELECT
     c.city_name AS "Ciudad",
     p.province_name AS "Provincia"
 FROM users u
-JOIN user_adresses ua ON u.id = ua.user_id
+JOIN user_addresses ua ON u.id = ua.user_id
 JOIN cities_enum c ON ua.city_id = c.id
 JOIN provinces_enum p ON ua.province_id = p.id;
 
@@ -184,9 +181,9 @@ CONCAT(w.first_name, ' ', w.first_surname) as "Creado Por",
 ise.status_name as "Estado"
 FROM users_incidents ui
 JOIN users u ON ui.user_id = u.id
-JOIN incident_user_enum iue ON ui.incident_user_id = iue.id
-JOIN significance_user_enum sue ON ui.significance_user_id = sue.id
-JOIN workers w ON ui.created_by_worker_id = w.id
+JOIN incidents_user_enum iue ON ui.incident_user_id = iue.id
+JOIN significances_user_enum sue ON ui.significance_user_id = sue.id
+JOIN workers w ON ui.reported_by_id = w.id
 JOIN incident_status_enum ise ON ui.incident_status_id = ise.id
 ORDER BY ui.created_at DESC;
 
@@ -198,7 +195,7 @@ SELECT
     rv.license_plate AS "Matrícula",
     COUNT(tru.user_id) AS "Total Pasajeros"
 FROM transport_routes tr
-JOIN route_shift_enum rs ON tr.route_shift_id = rs.id
+JOIN routes_shift_enum rs ON tr.route_shift_id = rs.id
 JOIN route_vehicles rv ON tr.route_vehicle_id = rv.id
 LEFT JOIN transport_routes_user tru ON tr.id = tru.route_id
 GROUP BY tr.route_number, rs.route_name, rv.license_plate;
@@ -208,30 +205,30 @@ GROUP BY tr.route_number, rs.route_name, rv.license_plate;
 SELECT
     CONCAT(u.first_name, ' ', u.first_surname) AS "Usuario",
     CONCAT(w.first_name, ' ', w.first_surname) AS "Registrado Por",
-    c_enum.clothes_name AS "Prenda",
+    c_enum.clothing_name AS "Prenda",
     b.created_at AS "Hora Entrada",
     b.comment AS "Observaciones"
 FROM user_belongings b
 JOIN users u ON b.user_id = u.id
 JOIN workers w ON b.worker_id = w.id
-JOIN user_clothing uc ON b.user_clothing_id = uc.id
-JOIN clothing_type_enum c_enum ON uc.clothes_id = c_enum.id;
+JOIN user_clothings uc ON b.user_clothing_id = uc.id
+JOIN clothing_types_enum c_enum ON uc.clothing_type_id = c_enum.id;
 
 
 SELECT
     CONCAT(u.first_name, ' ', u.first_surname) AS "Usuario",
-    c_enum.clothes_name AS "Ropa",
+    c_enum.clothing_name AS "Ropa",
     o_enum.object_name AS "Objeto",
     d_type.type AS "Tipo Pañal",
     ud.quantity AS "Cantidad"
 FROM user_belongings b
 JOIN users u ON b.user_id = u.id
-JOIN user_clothing uc ON b.user_clothing_id = uc.id
-JOIN clothing_type_enum c_enum ON uc.clothes_id = c_enum.id
+JOIN user_clothings uc ON b.user_clothing_id = uc.id
+JOIN clothing_types_enum c_enum ON uc.clothing_type_id = c_enum.id
 LEFT JOIN user_objects uo ON b.user_object_id = uo.id
-LEFT JOIN objects_type_enum o_enum ON uo.object_id = o_enum.id
+LEFT JOIN object_types_enum o_enum ON uo.object_type_id = o_enum.id
 LEFT JOIN user_diapers ud ON b.user_diaper_id = ud.id
-LEFT JOIN diapers_type_enum d_type ON ud.type_id = d_type.id;
+LEFT JOIN diaper_types_enum d_type ON ud.diaper_type_id = d_type.id;
 
 ------------------
 -- Tratamientos
@@ -251,7 +248,7 @@ LEFT JOIN user_illness_treatment_details uit ON ui.id = uit.user_illness_id
 LEFT JOIN treatment_details td ON uit.treatment_detail_id = td.id
 LEFT JOIN treatment_details_medication tdm ON td.id = tdm.treatment_detail_id
 LEFT JOIN medications m ON tdm.medication_id = m.id
-LEFT JOIN medication_name_enum mn ON m.medication_name_id = mn.id
+LEFT JOIN medication_names_enum mn ON m.medication_name_id = mn.id
 ORDER BY u.first_name;
 
 -- Alergias
@@ -276,7 +273,7 @@ JOIN user_allergies ua ON umi.id = ua.user_medical_info_id
 JOIN allergies_enum ae ON ua.allergy_id = ae.id
 JOIN allergies_medications am ON ua.id = am.user_allergy_id
 JOIN medications m ON am.medication_id = m.id
-JOIN medication_name_enum mn ON m.medication_name_id = mn.id;
+JOIN medication_names_enum mn ON m.medication_name_id = mn.id;
 
 -- Alergias (con y sin medicación)
 SELECT
@@ -291,7 +288,7 @@ JOIN allergies_enum ae ON ua.allergy_id = ae.id
 -- Aquí usamos LEFT JOIN para que no desaparezcan los que no toman nada
 LEFT JOIN allergies_medications am ON ua.id = am.user_allergy_id
 LEFT JOIN medications m ON am.medication_id = m.id
-LEFT JOIN medication_name_enum mn ON m.medication_name_id = mn.id;
+LEFT JOIN medication_names_enum mn ON m.medication_name_id = mn.id;
 
 ------------------
 -- Baños e Higiene
@@ -303,10 +300,10 @@ SELECT
     CONCAT(u.first_name, ' ', u.first_surname) AS Paciente,
     bk.task_name AS Tarea,
     bk.estimated_time AS Duracion
-FROM bathroom_schedule bs
+FROM bathroom_schedules bs
 JOIN users u ON bs.user_id = u.id
 JOIN bathroom_turns bt ON bs.bathroom_turn_id = bt.id
-JOIN bathroom_tasks bk ON bs.bathroom_task_id = bk.id
+JOIN bathroom_tasks_enum bk ON bs.bathroom_task_id = bk.id
 ORDER BY bt.start_at, u.first_name;
 
 -- Carga de trabajo por turno
@@ -315,6 +312,6 @@ SELECT
     bt.start_at AS Inicio,
     COUNT(bs.id) AS Total_Pacientes
 FROM bathroom_turns bt
-LEFT JOIN bathroom_schedule bs ON bt.id = bs.bathroom_turn_id
+LEFT JOIN bathroom_schedules bs ON bt.id = bs.bathroom_turn_id
 GROUP BY bt.id, bt.turn_name, bt.start_at
 ORDER BY bt.start_at;
