607fa57a1fdcbf2bce1ab2d340131eabce1a1ab1
Maria Elena Miranda <mara.mirandav@gmail.com>
2026-04-24 22:30:15 +0200

    Implementación CRUD completo de BathroomSchedule con repositorios, DTOs, mapper, servicio y controlador

---
+++ src/main/java/com/centros_sass/app/repository/BathroomScheduleRepository.java
@@ -0,0 +1,23 @@
+package com.centros_sass.app.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.centros_sass.app.model.bathroom.BathroomSchedule;
+
+@Repository
+public interface BathroomScheduleRepository extends JpaRepository<BathroomSchedule, Integer> {
+
+    Page<BathroomSchedule> findAllByIsActiveTrue(Pageable pageable);
+
+    Page<BathroomSchedule> findAllByIsActiveFalse(Pageable pageable);
+
+    Page<BathroomSchedule> findAllByUserId(Integer userId, Pageable pageable);
+
+    Page<BathroomSchedule> findAllByBathroomTurnId(Integer bathroomTurnId, Pageable pageable);
+
+    Page<BathroomSchedule> findAllByBathroomTaskId(Integer bathroomTaskId, Pageable pageable);
+
+}
